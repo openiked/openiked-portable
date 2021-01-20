0224031b7333ad6fb1f325919f02e52bce299815
David Linder <19273958+linderd@users.noreply.github.com>
2021-01-20 21:42:50 +0100

    Update run_test.sh

--- regress/parser-libfuzzer/run_test.sh
+++ regress/parser-libfuzzer/run_test.sh
@@ -5,7 +5,8 @@
 # ASAN-option to help finding the source of memory leaks
 export ASAN_OPTIONS=fast_unwind_on_malloc=0
 
-$(dirname "$0")/test_parser -dict=$(dirname "$0")/fuzz.dict -max_len=8164 -max_total_time=60 $(dirname "$0")/corpus
+$(dirname "$0")/test_parser -dict=$(dirname "$0")/fuzz.dict -max_len=8164 -max_total_time=60 -error_exitcode=0 $(dirname "$0")/corpus
 
+#TODO: remove error_exitcode=0 for github CI
 #maybe the corpus shold be on a persistent storage 
 #total_time is maybe a little bit short
