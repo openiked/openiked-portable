60f1a48e4498cc4593ba0a000e68f7418ad530c2
David Linder <19273958+linderd@users.noreply.github.com>
2021-01-20 19:31:23 +0100

    update run_test.sh with max duration

--- regress/parser-libfuzzer/run_test.sh
+++ regress/parser-libfuzzer/run_test.sh
@@ -5,6 +5,7 @@
 # ASAN-option to help finding the source of memory leaks
 export ASAN_OPTIONS=fast_unwind_on_malloc=0
 
-$(dirname "$0")/parse_test -dict=$(dirname "$0")/fuzz.dict -max_len=15000 $(dirname "$0")/corpus
+$(dirname "$0")/parse_test -dict=$(dirname "$0")/fuzz.dict -max_len=15000 -max_total_time=60 $(dirname "$0")/corpus
 
-#maybe the corpus should be on a ramdisk 
+#maybe the corpus shold be on a persistent storage 
+#total_time is maybe a little bit short
