9f2894f892fc8b719cd1de78f19b99213dad765e
Tobias Heider <tobias.heider@stusta.de>
2021-01-30 17:08:59 +0100

    Enable errors in fuzzer, ignore leaks for now.

--- regress/parser-libfuzzer/run_test.sh
+++ regress/parser-libfuzzer/run_test.sh
@@ -3,9 +3,9 @@
 # script to run the parser-fuzzer with the right options
 
 # ASAN-option to help finding the source of memory leaks
-export ASAN_OPTIONS=fast_unwind_on_malloc=0
+export ASAN_OPTIONS=fast_unwind_on_malloc=0:detect_leaks=0
 
-$(dirname "$0")/test_libfuzzer -dict=$(dirname "$0")/fuzz.dict -max_len=8164 -max_total_time=60 -error_exitcode=0 $(dirname "$0")/corpus
+$(dirname "$0")/test_libfuzzer -dict=$(dirname "$0")/fuzz.dict -max_len=8164 -max_total_time=60 $(dirname "$0")/corpus
 
 #TODO: remove error_exitcode=0 for github CI
 #maybe the corpus shold be on a persistent storage 
