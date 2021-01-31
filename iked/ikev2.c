b68600cb81082b17754ac75425e2140b1e652e98
Tobias Heider <tobias.heider@stusta.de>
2021-01-31 01:45:02 +0100

    Don't leak flows if ikev2_cp_fixflow() fails.

--- iked/ikev2.c
+++ iked/ikev2.c
@@ -5948,8 +5948,10 @@ ikev2_childsa_negotiate(struct iked *env, struct iked_sa *sa,
 			flowa->flow_local = &sa->sa_local;
 			flowa->flow_peer = &sa->sa_peer;
 			flowa->flow_ikesa = sa;
-			if (ikev2_cp_fixflow(sa, flow, flowa) == -1)
+			if (ikev2_cp_fixflow(sa, flow, flowa) == -1) {
+				flow_free(flowa);
 				continue;
+			}
 
 			skip = 0;
 			TAILQ_FOREACH(saflow, &sa->sa_flows, flow_entry) {
@@ -5976,8 +5978,11 @@ ikev2_childsa_negotiate(struct iked *env, struct iked_sa *sa,
 			    sizeof(flow->flow_dst));
 			memcpy(&flowb->flow_dst, &flow->flow_src,
 			    sizeof(flow->flow_src));
-			if (ikev2_cp_fixflow(sa, flow, flowb) == -1)
+			if (ikev2_cp_fixflow(sa, flow, flowb) == -1) {
+				flow_free(flowa);
+				flow_free(flowb);
 				continue;
+			}
 
 #if defined(HAVE_LINUX_IPSEC_H)
 			struct iked_flow	*flowc;
@@ -5997,8 +6002,12 @@ ikev2_childsa_negotiate(struct iked *env, struct iked_sa *sa,
 			    sizeof(flow->flow_dst));
 			memcpy(&flowc->flow_dst, &flow->flow_src,
 			    sizeof(flow->flow_src));
-			if (ikev2_cp_fixflow(sa, flow, flowc) == -1)
+			if (ikev2_cp_fixflow(sa, flow, flowc) == -1) {
+				flow_free(flowa);
+				flow_free(flowb);
+				flow_free(flowc);
 				continue;
+			}
 
 			TAILQ_INSERT_TAIL(&sa->sa_flows, flowc, flow_entry);
 #endif
