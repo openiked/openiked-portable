561e9f9fcdfd341b28c416c766411cf3908fe74c
tb <tb@openbsd.org>
2021-11-18 22:59:03 +0000

    iked: replace a conditional EVP_CIPHER_CTX_cleanup() + free() stanza with an unconditional EVP_CIPHER_CTX_free().
    ok tobhe

--- iked/crypto.c
+++ iked/crypto.c
@@ -1,4 +1,4 @@
-/*	$OpenBSD: crypto.c,v 1.35 2021/11/18 22:42:02 tobhe Exp $	*/
+/*	$OpenBSD: crypto.c,v 1.36 2021/11/18 22:59:03 tb Exp $	*/
 
 /*
  * Copyright (c) 2020-2021 Tobias Heider <tobhe@openbsd.org>
@@ -552,10 +552,7 @@ cipher_free(struct iked_cipher *encr)
 {
 	if (encr == NULL)
 		return;
-	if (encr->encr_ctx != NULL) {
-		EVP_CIPHER_CTX_cleanup(encr->encr_ctx);
-		free(encr->encr_ctx);
-	}
+	EVP_CIPHER_CTX_free(encr->encr_ctx);
 	ibuf_release(encr->encr_iv);
 	ibuf_release(encr->encr_key);
 	free(encr);
