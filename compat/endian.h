4316a760411cea20845b94ca9f0c36fb97f82bdc
Tobias Heider <tobias.heider@stusta.de>
2023-03-21 23:45:45 +0100

    FreeBSD does provide endian.h now, so fix the include.
    Found by David Marec

--- compat/endian.h
+++ compat/endian.h
@@ -44,8 +44,7 @@
 #endif
 #endif /* __linux__ */
 
-#if defined(__FreeBSD__) && !defined(HAVE_ENDIAN_H)
-#include <sys/endian.h>
+#if defined(__FreeBSD__)
 #if !defined(betoh16)
 #define betoh16	be16toh
 #endif
