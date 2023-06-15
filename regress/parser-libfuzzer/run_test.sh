de0cdfbdd2591415a486a07bc52beab86d55c688
linderd <19273958+linderd@users.noreply.github.com>
2023-06-15 09:45:04 +0200

    adapt fuzzing-pipeline to recent changes, make fuzzer easier to use

--- regress/parser-libfuzzer/run_test.sh
+++ regress/parser-libfuzzer/run_test.sh
@@ -6,5 +6,3 @@
 export ASAN_OPTIONS=fast_unwind_on_malloc=0
 
 $(dirname "$0")/test_libfuzzer -dict=$(dirname "$0")/fuzz.dict -max_len=8164 -max_total_time=300 $(dirname "$0")/corpus
-
-# maybe it would be a good idea to push the new corpus to another git-repo
